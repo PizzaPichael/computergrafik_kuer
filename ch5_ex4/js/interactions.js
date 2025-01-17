e27482b1f3acfac6ec0f9e692287991f077ed684
Micha <152519938+PizzaPichael@users.noreply.github.com>
2025-01-17 21:18:28 +0100

    feat: refactorized the interaction.js and added comments to all functions

--- ch5_ex4/js/interactions.js
+++ ch5_ex4/js/interactions.js
@@ -137,7 +137,7 @@ let isDragging = false;
  * If the first interssected object ist the cord object, the object is saved in the cordObject variable.
  * The selectedObject variable is set to the first intersected object.
  * The isDragging variable is set to true.
- * The mouseMoveSituation variable is set to "cord".
+ * The mouseMoveSituation variable is set to "cord", important for the {@link onMouseMove} function.
  * 
  * @param event The mousedown event.
  */
@@ -196,17 +196,28 @@ let instrumentsY = 2.5;
  * This is used for dragging the cord as well as dragging the instruments.
  * 
  * The function gets the mouse coordinates.
+ * 
  * If the mouseMoveSituation is "cord", the function checks if the isDragging variable is true.
+ * This is triggered by the {@link onMouseDown} function.
  * Otherwise the function returns.
- * If true, jsut like the {@link onMouseDown} function, the function uses a raycaster to get the cord object.
+ * If true, just like the {@link onMouseDown} function, the function uses a raycaster to get the cord object.
  * As long as the cord gets dragged and is the first intersected object, the function calculates the difference
  * between the current and the previous world y coordinate of the cord.
  * The cord object gets moved by the difference.
  * 
  * When the dragPlane ist reached and becomes the first intersected object:
  * - the overlay text gets removed
- * - the spotlights get activated
- * */
+ * - the spotlight gets activated
+ * - {@link startMovements} is called to start curtain and camera movements after two seconds
+ * 
+ * If the mouseMoveSituation is "instrument", the function uses a raycaster to get the intersected objects.
+ * This is triggered by the {@link onMouseClick} function.
+ * {@link onMouseClick} already established, that the object is one of the isntruments.
+ * The function moves the instrument to where the raycaster currently intersects the first intersected object
+ * behind the instrument, which is the instrumentDragPlane.
+ * 
+ * @param event The mousemove event.
+ */
 function onMouseMove(event) {
     const mouseCoords = getMouseCoords(event);
     if(mouseMoveSituation == "cord") {
@@ -236,10 +247,8 @@ function onMouseMove(event) {
             if (firstIntersectedObject.name === "dragPlane" && allIntersectedObjects[1].object.name === "cord") {
                 updateOverlayText(''); // Remove overlay text
                 activateSpotlight(scene, instrumentActivationPlane);
-                spotLight = getSpotlight();
+                spotLight = getSpotlight(); // Init spotlight variable
                 setTimeout(startMovements(), 2000);
-                //console.log("moveCurtains set by dragPlane contact", moveCurtains);
-                //Hier Event einfügen
             }
         }
     } else if(mouseMoveSituation == "instrument") {
@@ -250,6 +259,7 @@ function onMouseMove(event) {
             const intersect = intersects[0];
             const worldPoint = intersect.point;
 
+            //Has to be done individually because .position is read only somehow
             instrumentToMove.position.x = worldPoint.x;
             instrumentToMove.position.y = instrumentsY;
             instrumentToMove.position.z = worldPoint.z;
@@ -263,20 +273,44 @@ function onMouseMove(event) {
 }
 
 // Funktion zum Stoppen der Kordelbewegung
+/**
+ * Function to handle the mouseup event.
+ * This function stops the dragging of the cord.
+ * If the cord was dragged, the cord gets reset to its initial y position.
+ * Does nothing when not dragging or no object selected
+ * 
+ * @param event The mouseup event.
+ */
 function onMouseUp(event) {
-    //TODO fix the cord reset
-    if (!isDragging || !selectedObject) return;
+    if (!isDragging || !selectedObject) return; 
+
     if (initCordYPos != cordObject.position.y) {
-        mouseMoveSituation = "";
+        mouseMoveSituation = "";    // Clear the mouseMoveSituation
         cordObject.position.y = initCordYPos;
     }
 
-    // Stoppe die Bewegung der Kordel
+    // Stop movement of the cord
     isDragging = false;
     selectedObject = null;
 }
 
 let amountOfClicks = 0;
+/**
+ * Function to handle the mouseclick event.
+ * This function is used to select the instruments.
+ * The function only works, if the theaterroom is hidden.
+ * The function counts the amount of clicks as a way of ensuring that:
+ * - the first click is instrument selection 
+ * - the second click is the one to let go of the instrument
+ * 
+ * The function uses a raycaster to get the intersected instrument.
+ * If an instrument is intersected, the function creates a dragplane for the instrument to be dragged along.
+ * The dragplane ist created temporalily after instrument selecetion, to stop it from interfering when trying 
+ * to select an isntrument from underneath the dragplane.
+ * When the instrument is let go, so amountOfClicks != 1, the dragplane gets removed.
+ * 
+ * @param event The mouseclick event.
+ */ 
 function onMouseClick(event) {
     if(theaterRoomHidden) {
         amountOfClicks++;
@@ -310,9 +344,12 @@ function onMouseClick(event) {
     }
 }
 
-
-
-//Start movements
+/**
+ * Function to start the movements of the curtains and the camera.
+ * The function sets the moveCurtains variable to true, to start the curtain movement.
+ * The function sets a timeout of 5 seconds to start the camera movement.
+ * The function is called by the {@link onMouseMove} function, when the dragplane is reached.
+ */
 function startMovements() {
     moveCurtains = true;  // Starte die Vorhangbewegung sofort
     
@@ -322,32 +359,67 @@ function startMovements() {
     }, 5000);
 }
 
-//Function to move the curtains
+//----Global variables for curtain and camera movement----
 let curtainMovementSpeed = 0.03;
 let curtainRightEndPosition = 62;
 let curtainLeftEndPosition = -28;
 var cameraInFinalPosition = false;
 
+/**
+ * Function to move the curtains.
+ * The function is continously called by the draw() function inside the {@link render} file.
+ * This should initially allow a smooth movement of the curtains.
+ * Would propably be done different now.
+ * 
+ * The funtion only moves the curtains, if the moveCurtains variable is true.
+ * {@link startMovements} sets the moveCurtains variable to true.
+ * 
+ * The function gets the current position of the left and right curtain.
+ * The function checks if the curtains are not at their end position.
+ * If the curtains are not at their end position, the function moves the curtains by the curtainMovementSpeed.
+ */
 export async function moveCurtainsFunction() {
     var curtainLeft = await getCurtainLeft();
     var curtainRight = getCurtainRight();
     if (!moveCurtains) return;
     
-    // Berechne die Differenz zwischen der aktuellen und der Zielposition
     if (curtainRight.position.x < curtainRightEndPosition && curtainLeft.position.x > curtainLeftEndPosition) {
         curtainRight.position.x += curtainMovementSpeed;
         curtainLeft.position.x -= curtainMovementSpeed;
     } 
 }
 
-//Function to move camera after Curtain movement
+//----Global variables for camera movement----
 let cameraMovementSpeed = 0.2;
 let cameraEndPosition = 45;
 let theaterRoomHidden = false;
 
+/**
+ * Function to move the camera forward.
+ * The function is continously called by the draw() function inside the {@link render} file.
+ * This should initially allow a smooth movement of the camera.
+ * Would propably be done different now.
+ * 
+ * The funtion only moves the camera, if the moveCamera variable is true.
+ * {@link startMovements} sets the moveCamera variable to true.
+ * 
+ * The function gets the current position of the camera.
+ * The function checks if the camera is not at its end position.
+ * If the camera is not at its end position, the function moves the camera by the cameraMovementSpeed.
+ * 
+ * If the camera reached its end position and the theater room is still visible the function calls
+ * the {@link hideTheaterroom} function to hide the theater room.
+ * 
+ * If the camera reached its end position, the function stops the camera movement and the curtain movement.
+ * The function sets the {@link moveCamera} and {@link moveCurtains} variables to false.
+ * The function sets the {@link cameraInFinalPosition} variable to true.
+ * The function calls the {@link panCameraToZero} function to pan the camera to zero.
+ * The function calls the {@link setupGui} function in the gui.js to set up the gui.
+ * 
+ * When the camera is panned to zero, the function calls the {@link enableCameraMovement} function to enable the camera movement.
+ */
 export async function moveCameraForward() {
     if(cameraInFinalPosition && !theaterRoomHidden) {
-        console.log("Calling hideTheaterroom...");
         hideTheaterroom();
     }
 
@@ -356,45 +428,57 @@ export async function moveCameraForward() {
     if (camera.position.z > cameraEndPosition) {
         camera.position.z -= cameraMovementSpeed;
     } else {
-        moveCamera = false;  // Stoppe die Bewegung, wenn das Ziel erreicht ist
-        moveCurtains = false; // Stoppe die Vorhangbewegung
+        moveCamera = false;  
+        moveCurtains = false;
         cameraInFinalPosition = true;
         panCameraToZero();
         setupGui();
         if(cameraPanned) {
-            console.log("Current camera position: ", camera.position);
-            console.log("Current camera rotation: ", camera.rotation);
             setTimeout(() => {
-                console.log("Calling enableCameraMovement...");
                 enableCameraMovement();
             },2000);
         }
     }
 }
 
+//----Global variable for camera panning----
 let cameraPanned = false;
+/**
+ * Function to pan the camera to the zero coordinates.
+ * This was nessesary, as {@link initTrackballControls} for some reasons automaticals rotates the camera to this angle.
+ * This would cause an abrupt pan, when trackballcontrols got activated.
+ * This functions places the pan at the end of the camera movement, which makes it not noticable.
+ * The function only pans the camera, if the camera is in its final position and the camera is not already panned.
+ * Then sets the {@link cameraPanned} variable to true.
+ */ 
 export function panCameraToZero() {
     if(cameraInFinalPosition && !cameraPanned) {
         let angleToAchieve =-0.38050637711236873;
         camera.rotation.x = angleToAchieve;
-        while(camera.rotation.x > angleToAchieve) {
-            camera.rotation.x -= 0.001;
-            setTimeout(() => {
-            }, 500);       }
         cameraPanned = true; 
     }
 }
 
-// Funktion zum Entfernen eines Objekts aus der Szene geschrieben von Copilot
+/**
+ * Function to remove an object from the scene.
+ * This function was proposed by Copilot.
+ * 
+ * It removes the object from its parent, if the object exists and has a parent.
+ * 
+ * @param object The object to remove from the scene.
+ */
 function removeObjectFromScene(object) {
     if (object && object.parent) {
         object.parent.remove(object);
     }
 }
 
-//Hide Theaterroom
+/**
+ * Function to hide the theater room.
+ * The function removes all objects that are part of the theater room.
+ * The function sets the {@link theaterRoomHidden} variable to true.
+ */
 function hideTheaterroom() {
-    console.log("Hiding theaterroom...");
     removeObjectFromScene(theaterRoom);
     removeObjectFromScene(theaterChairs);
     removeObjectFromScene(curtainLeft);
@@ -410,22 +494,49 @@ function hideTheaterroom() {
     theaterRoomHidden = true;
 }
 
+/**
+ * This function is used to rotate the portal plane on its z-axis and make the portal seem more dynamic.
+ * The function is continously called by the draw() function inside the {@link render} file.
+ */ 
 export function rotatePortal() {
     portalPlane.rotation.z -= 0.01;
 }
 
 //----Enable camera control by mouse----
+/**
+ * Function to enable the camera movement.
+ * The function initialises the trackball controls for the camera.
+ * The function calls the {@link explainControls} function to explain the controls to the user, after camera movement is enabled.
+ */ 
 export function enableCameraMovement() {
     if(!trackballControls) {
-        console.log("Enabling CameraMovement")
         trackballControls = initTrackballControls(camera, gl);
-        console.log("Camera rotation after enabling movement: ", camera.rotation);        
         explainControls('all');
     }
     
 }
 
+//----Initialising global variable for the explainControls function----
 let controlExplanationAdded = false;
+/**
+ * Function to explain the controls to the user.
+ * The function is called by the {@link enableCameraMovement} function.
+ * It can also be calles by the gui. See {@link setupGui} in the gui.js.
+ * 
+ * The fucntion is async, to allow the overlay text to be updated with a delay and not be removed
+ * until the desired time ist run down.
+ * 
+ * The function updates the overlay text with the {@link updateOverlayText} function.
+ * The function explains the controls for the mouse, the music and the instruments.
+ * 
+ * The function can explain all controls at once or only the controls for the mouse, the music or the instruments,
+ * depending in the {@link controlType} parameter.
+ * 
+ * The function adds the controls to call itself to the gui once, if they have not been added yet.
+ * When the have been added, the function sets the {@link controlExplanationAdded} variable to true.
+ * 
+ * @param controlType The type of controls to explain. Can be 'mouse', 'music', 'instruments' or 'all'.
+ */
 export async function explainControls(controlType) {
     console.log("Explaining controls...");
     if(controlType == 'mouse' || controlType == 'all') {
@@ -449,15 +560,35 @@ export async function explainControls(controlType) {
     }
 }
 
+/**
+ * Getter for the trackball controls.
+ * This function was taken from the last exercise we did.
+ * @returns The trackball controls.
+ */
 export function getTrackBallControls() {
-    //console.log("Called getTrackBallControls")
     return trackballControls;
 }
 
+/**
+ * Updates the trackball controls.
+ * This function was taken from the last exercise we did.
+ * 
+ * @param clockInput The clock input for the trackball controls.
+ */
 export function updateTrackBallControls(clockInput) {
     trackballControls.update(clockInput);
 }
 
+/**
+ * Helper function to check if an instrument is within the activation plane.
+ * 
+ * The function calculates the distance between the object and the activation plane.
+ * If the distance is smaller or equal to the radius of the activation plane, the function returns true.
+ * Otherwise the function returns false.
+ * 
+ * @param object The object/instrument to check if it is within the activation plane.
+ * @returns True if the object is within the activation plane, false otherwise.
+ */
 function isObjectWithinActivationPlane(object) {
     const planePosition = instrumentActivationPlane.position;
     const planeRadius = instrumentActivationPlane.geometry.parameters.radius;
@@ -470,6 +601,14 @@ function isObjectWithinActivationPlane(object) {
     return distance <= planeRadius;
 }
 
+/**
+ * Function to check if the instruments are within the activation plane.
+ * The function calls the {@link isObjectWithinActivationPlane} helper function for each instrument.
+ * The function updates the status of the instruments with the {@link updateStatus} function
+ * from the gui.js file.
+ * The function is continously called by the {@link render} file in the draw() function.
+ * The function only checks the instruments, if the camera is in its final position.
+ */
 export function checkInstrumentsPosition() {
     if(cameraInFinalPosition) {
         if(isObjectWithinActivationPlane(cello)) {
@@ -494,8 +633,16 @@ export function checkInstrumentsPosition() {
 
 }
 
+/**
+ * Function to update the overlay text.
+ * Updates the html element with the id 'overlay' with the given text.
+ * 
+ * @param text The text to update the overlay with.
+ * @param delay If the overlay text should be delayed. Standard is false.
+ * @param delaytime The time to delay the overlay text. Standard is 0.
+ * @returns A promise to ensure, that the overlay text is updated and stays visible for the desired time.
+ */
 function updateOverlayText(text, delay=false, delaytime=0) {
-    console.log("Updating overlay text: ", text);
     return new Promise((resolve, reject) => {
         try {
             const overlay = document.getElementById('overlay');
@@ -519,13 +666,17 @@ function updateOverlayText(text, delay=false, delaytime=0) {
     });
 }
 
-// Funktion zum Ein- und Ausblenden des Overlays
+/**
+ * Function to toggle the overlay.
+ * The function shows the html element with the id 'overlay' if the visible parameter is true by setting 
+ * its display style to 'block'.
+ * The function hides the html element with the id 'overlay' if the visible parameter is false by setting
+ * its display style to 'none'.
+ * @param visible If the overlay should be visible or not.
+ */
 function toggleOverlay(visible) {
     const overlay = document.getElementById('overlay');
     if (overlay) {
         overlay.style.display = visible ? 'block' : 'none';
     }
-    console.log("Overlay toggled: ", overlay.style.display);
 }
-
-//TODO add Spotlights to the instrumnnts when they are on stage
